97218f099f2d029032a802e7d79b24b7a029e314
Aendera <chaosobscura@gmail.com>
2020-06-12 12:10:52 +0200

    Completed Virtual Die project
    Virtual Die can now choose number of rolls and of die faces based on user input
    Virtual Die returns the rolls, the average of the rolls and the maximum and minumum rolls

--- C++/WorkthroughC++ByExample/VirtualDie/src/main.cpp
+++ C++/WorkthroughC++ByExample/VirtualDie/src/main.cpp
@@ -1,23 +1,47 @@
 #include <iostream>
 #include <random>
 
-int roll_dice() {
+int roll_dice(int number_of_faces) {
 	std::random_device random_device;
 	std::default_random_engine random_engine(random_device());
-	std::uniform_int_distribution<int> uniform_int_distribution(1, 6);
+	std::uniform_int_distribution<int> uniform_int_distribution(1, number_of_faces);
 	auto random_int = uniform_int_distribution(random_engine);
 	return random_int;
 
 }
 int main(int argc, char* argv[]) {
-	double average = 0.0;
-	for (int i = 0; i < 20; i++)
+
+	int number_rolls = 10;
+	std::cout << "Enter number of times to roll : ";
+	std::cin >> number_rolls;
+	printf("Rolling die %d times \n", number_rolls);
+	int number_of_faces = 6;
+	std::cout << "Enter number of faces : ";
+	std::cin >> number_of_faces;
+	printf("Dice will have %d faces \n", number_of_faces);
+
+	//Setting max_roll and min_roll to values 
+	//just below/above possible dice values
+	//This guarantees max_roll/min_roll are actual rolled values
+	auto max_roll = 0;
+	auto min_roll = number_of_faces + 1;
+	auto sum=0;
+	for (int i = 0; i < number_rolls; i++)
 	{
-		auto random_int = roll_dice();
-		printf("You rolled a %d on roll %d of 20\n", random_int, i+1);
-		average += (double) random_int;
+		auto random_int = roll_dice(number_of_faces);
+		printf("You rolled a %d on roll %d of %d\n", random_int, i+1, number_rolls);
+		sum += random_int;
+		if (random_int > max_roll) {
+			max_roll = random_int;
+		}
+		if (random_int < min_roll) {
+			min_roll = random_int;
+		}
 	}
-	average /= 20.0;
-	printf("Your average roll was %f", average);
+
+	double average = (double) sum/number_rolls;
+	printf("Your average roll was %f\n", average);
+	printf("Your maximum roll was %d\n", max_roll);
+	printf("Your minimum roll was %d\n", min_roll);
 	return 0;
 }
