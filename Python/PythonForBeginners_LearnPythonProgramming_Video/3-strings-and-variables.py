92f891f18d6be2ffe91ce28ba1d3d7088f15bd2d
Aendera <chaosobscura@gmail.com>
2020-06-13 21:33:18 +0200

    Completed first Python Exercises
    Exercises on strings-and-variables

---
+++ Python/PythonForBeginners_LearnPythonProgramming_Video/3-strings-and-variables.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 13 21:25:56 2020
+
+@author: user
+"""
+
+#Exercise 1
+animal="cat"
+vegetable="bell pepper"
+mineral="iron"
+
+print("Here is an animal, a vegetal, and a mineral")
+print(animal)
+print(vegetable)
+print(mineral)
+
+#Exercise 2
+print()
+user_input=input("Please type something and press Enter:\n")
+print("You entered:")
+print(user_input)
+print()
+
+#Exercise_3
+#Keep input of previous exercises
+print("       {}".format("_"*len(user_input)))
+print("     < {} >".format(user_input))
+print("       {}".format("-"*len(user_input)))
+print("        /")
+print(" /\_/\ /")
+print("( o.o )")
+print(" > ^ <")
--- Python/PythonForBeginners_LearnPythonProgramming_Video/revision.py
+++ Python/PythonForBeginners_LearnPythonProgramming_Video/revision.py
@@ -2,7 +2,17 @@
 """
 Created on Sat Jun 13 19:27:33 2020
 
-@author: user
+@author: Romain Bettinelli
 """
 #This is a placeholder
 print("Hello World!")
+
+#Section 3 Lesson Samples
+print('I ' + 'love ' + 'Python.')
+print('I' + 'love' + 'Python.')
+print('happy'*3)
+print("{} {} {}".format("I","love","Python"))
+print("I {0} {1}. {1} {0}s me.".format("love", "Python"))
+
+#Section 3 Exercises - Variables and Strings
+#Exercise 1
