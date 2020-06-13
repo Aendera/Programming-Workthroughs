3717a7eb587885ab38639c39abf768b154dcbedf
Aendera <chaosobscura@gmail.com>
2020-06-13 16:02:56 +0200

    Completed DataStructuresAlgorithms project
    Created a simple phonebook using tuples, made functions for sorting phonebook, and made function to find people whose first names start with B

--- C++/WorkthroughC++ByExample/SortingApplication/src/main.cpp
+++ C++/WorkthroughC++ByExample/SortingApplication/src/main.cpp
@@ -1,8 +1,78 @@
+#include <ostream>
 #include <iostream>
+#include<vector>
+#include<algorithm>
+#include <iterator>
+#include <numeric>
+#include<string>
+using contact = std::tuple<std::string, std::string,std::string>;
+using contacts = std::vector<contact>;
 
-int main(int argc, char* argv[])
+contacts demo_contacts =
 {
-	
+	{"Javon", "Bernard","555-667-2345"},
+	{"Kiana", "Lester","234-745-4534"},
+	{"Lia", "Badwin","546-234-7578"},
+	{"Ryleigh", "Liu","094-245-7748"},
+	{"Shea", "Gibbs","251-647-8941"},
+	{"Sabrina", "Galvan","345-798-3168"},
+	{"Glenn", "Henry","654-984-8664"},
+	{"Lucia", "Wright","134-798-6464"},
+	{"Annika", "Carr","461-641-7497"},
+	{"Bruno", "Frank","465-065-4610"}
+};
 
+inline std::ostream& operator<<(std::ostream& output, const contact& person)
+{
+	output << std::get<1>(person)<<","<<std::get<0>(person) << ": " << std::get<2>(person);
+	return output;
+}
+bool compare_by_first_name(const contact& first, const contact& second) {
+	return std::get<0>(first) < std::get<0>(second);
+}
+
+bool compare_by_last_name(const contact& first, const contact& second) {
+	return std::get<1>(first) < std::get<1>(second);
+}
+
+bool compare_by_phone_number(const contact& first, const contact& second) {
+	return std::get<2>(first) < std::get<2>(second);
+}
+
+void print_contacts(const contacts& list) {
+	std::for_each(list.begin(), list.end(),
+		[](const contact& contact) {
+			std::cout << contact << std::endl;
+		});
+}
+int main(int argc, char* argv[])
+{	
+	std::cout << "Sort by first name..." << std::endl;
+	std::sort(demo_contacts.begin(), demo_contacts.end(),compare_by_first_name);
+	print_contacts(demo_contacts);
+	std::cout << std::endl;
+
+
+	std::cout << "Sort by last name..." << std::endl;
+	std::sort(demo_contacts.begin(), demo_contacts.end(), compare_by_last_name);
+	print_contacts(demo_contacts);
+	std::cout << std::endl;
+
+
+	std::cout << "Sort by phone number..." << std::endl;
+	std::sort(demo_contacts.begin(), demo_contacts.end(), compare_by_phone_number);
+	print_contacts(demo_contacts);
+	std::cout << std::endl;
+
+
+	std::cout << "Searching for people..." << std::endl;
+	std::vector<contact> people_with_b;
+	std::copy_if(demo_contacts.begin(), demo_contacts.end(),
+		std::back_inserter(people_with_b), [](const contact& person) {
+			auto first_name = std::get<0>(person);
+			return first_name.find('B') != std::string::npos;
+		}
+	);
+	print_contacts(people_with_b);
 	return 0;
 }
