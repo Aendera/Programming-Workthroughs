186418056bb82c4a3ffc05c2ffb0f6654fed1a7c
Aendera <chaosobscura@gmail.com>
2020-06-14 08:21:31 +0200

    Completed and checked python Exercises

---
+++ Python/PythonForBeginners_LearnPythonProgramming_Video/10-files.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+ #Exercise 1
+ 
+with open('file.txt') as file:
+    count=0
+    for line in file:
+         count+=1
+         print("{}: ".format(count),line,end="")
+ #Exercise 2
+with open('animals.txt') as file:
+    sorted_animals=sorted(file.readlines())
+
+with open('sorted_animals.txt','w') as file:
+    file.writelines(sorted_animals)
