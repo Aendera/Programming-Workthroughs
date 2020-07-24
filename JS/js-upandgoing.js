45d29b41083bf416e77788310a6678aa530924c5
Aendera <chaosobscura@gmail.com>
2020-07-24 14:23:29 +0200

    Completed YouDontKnowJS Up and Going Exercise

---
+++ JS/js-upandgoing.js
@@ -0,0 +1,30 @@
+ //needs to be run somewhere with window,like a browser, to use prompt
+ const TAX_RATE=0.08;
+ const PHONE_PRICE=99.99;
+const SPENDING_THRESHOLD=200;
+const ACCESSORY_PRICE=9.99;
+ 
+ var balance=Number(prompt("Enter your balance"));
+amount=0;
+function calculateTax(price){
+    return price*TAX_RATE;
+}
+
+function formatAmount(price){
+    return "$"+price.toFixed(2);
+}
+
+while (amount<balance){
+    amount+=PHONE_PRICE;
+    if (amount<SPENDING_THRESHOLD){
+        amount+=ACCESSORY_PRICE;
+    }
+
+}
+amount+=calculateTax(amount);
+
+console.log("Your purchase"+formatAmount(amount));
+
+if (amount>balance){
+    console.log("You can't afford this purchase");
+}
