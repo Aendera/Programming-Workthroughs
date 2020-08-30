0b5d181d4e5da374121b3c59a6579e3469662878
Aendera <chaosobscura@gmail.com>
2020-08-30 18:57:28 +0200

    Completed Lesson part of Chapter 4- Adding Menus and Preferences to an Android App of Learn Java The Easy Way - No Starch Press

--- Java/LearnJavaTheEasyWay_NoStarchPress/AndroidProjects/GuessingGame/app/src/main/java/com/example/guessinggame/MainActivity.java
+++ Java/LearnJavaTheEasyWay_NoStarchPress/AndroidProjects/GuessingGame/app/src/main/java/com/example/guessinggame/MainActivity.java
@@ -1,14 +1,17 @@
 package com.example.guessinggame;
 
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.preference.PreferenceManager;
 import android.view.KeyEvent;
 import android.view.View;
 
@@ -19,12 +22,16 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import static android.preference.PreferenceManager.*;
+
 public class MainActivity extends AppCompatActivity {
     private EditText txtGuess;
     private Button btnGuess;
     private TextView lblOutput;
     private int theNumber;
     private int numberOfTries;
+    private int range = 100;
+    private TextView lblRange;
 
     public void checkGuess(){
         String guessText = txtGuess.getText().toString();
@@ -42,10 +49,15 @@ public class MainActivity extends AppCompatActivity {
                 newGame();
                 //btnPlayAgain.setVisible(true);
                 //btnGuess.setVisible(false);
+                SharedPreferences preferences = getDefaultSharedPreferences(this);
+                int gamesWon = preferences.getInt("gamesWon", 0) + 1;
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.putInt("gamesWon", gamesWon);
+                editor.apply();
             }
 
         } catch (Exception e) {
-            message = "Enter a whole number between 1 and 100.";
+            message = "Enter a whole number between  1 and " + range + ".";
         } finally {
             lblOutput.setText(message);
             txtGuess.requestFocus();
@@ -54,7 +66,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void newGame(){
-        theNumber = (int)(Math.random() * 100 + 1);
+        theNumber = (int)(Math.random() * range + 1);
+        String lbl = "Enter a number between 1 and " + range + ".";
+        lblRange.setText(lbl);
+        String firstGuess = "" + range/2;
+        txtGuess.setText(firstGuess);
+        txtGuess.requestFocus();
+        txtGuess.selectAll();
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,6 +82,9 @@ public class MainActivity extends AppCompatActivity {
         txtGuess = (EditText) findViewById(R.id.txtGuess);
         btnGuess = (Button) findViewById(R.id.btnGuess);
         lblOutput = (TextView) findViewById(R.id.lblOutput);
+        lblRange = (TextView) findViewById(R.id.textView4) ;
+        SharedPreferences preferences = getDefaultSharedPreferences(this);
+        range = preferences.getInt("range", 100);
         newGame();
         btnGuess.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -103,13 +124,77 @@ public class MainActivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch(item.getItemId()){
+            case R.id.action_settings:
+                final CharSequence[] items = {"1 to 10", "1 to 100", "1 to 1000"};
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setItems(items,null);
+                builder.setTitle("Select the range:");
+                builder.setItems(items, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int item) {
+                        switch(item){
+                            case 0:
+                                range = 10;
+                                storeRange(10);
+                                newGame();
+                                break;
+                            case 1:
+                                range = 100;
+                                storeRange(100);
+                                newGame();
+                                break;
+                            case 2:
+                                range = 1000;
+                                storeRange(1000);
+                                newGame();
+                                break;
+                        }
+                        dialog.dismiss();
+                    }
+                });
+                AlertDialog alert = builder.create();
+                alert.show();
+                return true;
+            case R.id.action_newgame:
+                newGame();
+                return true;
+            case R.id.action_gamestats:
+                SharedPreferences preferences = getDefaultSharedPreferences(this);
+                int gamesWon = preferences.getInt("gamesWon", 0);
+                AlertDialog statDialog = new AlertDialog.Builder(MainActivity.this).create();
+                statDialog.setTitle("Guessing Game Stats");
+                statDialog.setMessage(("You have won " + gamesWon + " games. Way to go!"));
+                statDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+                statDialog.show();
+                return true;
+            case R.id.action_about:
+                AlertDialog aboutDialog = new AlertDialog.Builder(MainActivity.this).create();
+                aboutDialog.setTitle("About Guessing Game");
+                aboutDialog.setMessage("(c)2020 Romain Bettinelli");
+                aboutDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int i) {
+                                dialog.dismiss();
+                            }
+                        });
+                aboutDialog.show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
+    }
 
-        return super.onOptionsItemSelected(item);
+    public void storeRange(int newRange){
+        SharedPreferences preferences = getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putInt("range", newRange);
+        editor.apply();
     }
 }
